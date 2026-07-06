205ec4f6fa29c32c320e9416beb66cafa2121c43
gilhdz320-prog <2.78204607e+08+gilhdz320-prog@users.noreply.github.com>
2026-07-06 08:45:59 +0000

    feat: add Meta Pixel (1476180747591915) to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import {
   Fraunces,
   Inter,
@@ -55,6 +56,28 @@ export default function RootLayout({
       lang="es"
       className={`${fraunces.variable} ${inter.variable} ${jetbrains.variable} ${cormorant.variable} h-full antialiased`}
     >
+      <head>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1476180747591915');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display:'none'}}
+            src="https://www.facebook.com/tr?id=1476180747591915&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className="min-h-full flex flex-col">
         <LocaleProvider>
           <LanguageToggle className="fixed right-4 top-4 z-[60]" />
